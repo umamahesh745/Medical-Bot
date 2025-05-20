ba4c26f8a80ac618df8a6d2d378c66a30a9bbac0
umamahesh745 <maheshmokka97@gmail.com>
2025-05-20 21:00:44 +0530

    Your commit message

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="GENERATIVE-AI-PROJECT",  # your package name
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[ 
+        # your dependencies
+    ],
+    # Optional but recommended:
+    author="UMA MAHEHSH",
+    description="Medical Bot package",
+    # etc.
+)
